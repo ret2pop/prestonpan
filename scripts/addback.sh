0e5eccfb37fb8ce26c50064c2ddf04f893694207
Preston Pan <preston@nullring.xyz>
2022-11-11 14:31:12 -0800

    added rss feed to main page

---
+++ scripts/addback.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# A simple extension to add a back button to every page that needs one.
+
+PROJECT_ROOT="./build/website"
+if [ "$(realpath "$1")" = "$(realpath "$PROJECT_ROOT/index.html")" ]; then
+	printf ' '
+	exit 0
+fi
+
+#
+if [ "$(basename "$1")" = 'index.html' ];
+then
+	printf '<a href="\.\./index\.html">Back</a>'
+else
+	printf '<a href="\./index\.html">Back</a>'
+fi
