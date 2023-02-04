8cc4983e38e3a35f6aa71d59ba377e9bce0dfeea
Preston Pan <preston@nullring.xyz>
2023-02-04 13:19:15 -0800

    update script yet again

--- finalize_build.sh
+++ finalize_build.sh
@@ -5,10 +5,10 @@ ROOT_DIR="./build/website/"
 for i in $(find "$ROOT_DIR" -type f -name '*.html'); do
 	if [ "$(basename "$i")" = 'index.html' ];
 	then
-		gsed -i -e 's|<!--BACKDIR-->|\| <a href="\.\./index\.html">Back</a>|g' "$i"
+		sed -i -e 's|<!--BACKDIR-->|\| <a href="\.\./index\.html">Back</a>|g' "$i"
 	else
-		gsed -i -e 's|<!--BACKDIR-->|\| <a href="\./index\.html">Back</a>|g' "$i"
+		sed -i -e 's|<!--BACKDIR-->|\| <a href="\./index\.html">Back</a>|g' "$i"
 	fi
 done;
 
-gsed -i -e 's|\| <a href="\.\./index\.html">Back</a>||g' "$ROOT_DIR/index.html"
+sed -i -e 's|\| <a href="\.\./index\.html">Back</a>||g' "$ROOT_DIR/index.html"
