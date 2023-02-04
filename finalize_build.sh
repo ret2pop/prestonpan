0952e317492a32cab3a1c37c6657b090468cb19b
Preston Pan <preston@nullring.xyz>
2023-02-04 13:10:27 -0800

    hello?

--- finalize_build.sh
+++ finalize_build.sh
@@ -5,10 +5,10 @@ ROOT_DIR="./build/website/"
 for i in $(find "$ROOT_DIR" -type f -name '*.html'); do
 	if [ "$(basename "$i")" = 'index.html' ];
 	then
-		sed -i -e 's|<!--BACKDIR-->|\| <a href="\.\./index\.html">Back</a>|g' "$i"
+		gsed -i -e 's|<!--BACKDIR-->|\| <a href="\.\./index\.html">Back</a>|g' "$i"
 	else
-		sed -i -e 's|<!--BACKDIR-->|\| <a href="\./index\.html">Back</a>|g' "$i"
+		gsed -i -e 's|<!--BACKDIR-->|\| <a href="\./index\.html">Back</a>|g' "$i"
 	fi
 done;
 
-sed -i -e 's|\| <a href="\.\./index\.html">Back</a>||g' "$ROOT_DIR/index.html"
+gsed -i -e 's|\| <a href="\.\./index\.html">Back</a>||g' "$ROOT_DIR/index.html"
